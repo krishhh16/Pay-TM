83587742977f67e85374d4f6dd0e6c9312088b05
krishhh16 <krishh16@yahoo.com>
2024-03-25 14:38:20 +0530

    added prisma db support

--- apps/pay-client/app/page.tsx
+++ apps/pay-client/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@repo/ui/button";
+import {PrismaClient} from "@repo/db/client"
 
-
+const client = new PrismaClient();
 export default function Page(): JSX.Element {
   return (
     <div className="text-2xl ">
