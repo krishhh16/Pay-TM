de506c7c8cb960f2ef496fa7e271dfc201038b08
krishhh16 <krishh16@yahoo.com>
2024-03-26 15:45:34 +0530

    added boilerplate next code with client and merchent user interface

--- apps/merchant-app/tailwind.config.js
+++ apps/merchant-app/tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
-   ],
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
