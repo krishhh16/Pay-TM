961dc4c1bd92ca7f31f14ddd80620328c7ae33f2
zeref <krishh16@yahoo.com>
2024-04-05 12:07:27 +0530

    added p2p transaction logic

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,9 @@
+import { AddMoney } from "../../../components/AddMoneyCard";
+
+export default function () {
+    return (
+        <div className="w-full">
+            <AddMoney/>
+        </div>
+    )
+}
